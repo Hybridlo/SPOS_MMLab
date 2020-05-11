8a7e84f181bd0309347f4938699cd48c200cb17c
Павел <pashanoy@gmail.com>
2020-05-11 21:10:32 +0300

    Possibly finished version

--- PageFault.java
+++ PageFault.java
@@ -8,7 +8,6 @@
   // Algorithm as described in the Memory Management section.
 
 import java.util.*;
-import Page;
 
 public class PageFault {
 
@@ -50,40 +49,57 @@ public class PageFault {
    * @param controlPanel represents the graphical element of the 
    *   simulator, and allows one to modify the current display.
    */
-  public static void replacePage ( Vector mem , int virtPageNum , int replacePageNum , ControlPanel controlPanel ) 
-  {
-    int count = 0;
-    int oldestPage = -1;
-    int oldestTime = 0;
-    int firstPage = -1;
-    int map_count = 0;
-    boolean mapped = false;
+  static final int TAU = 100;
+  static int pointer = -1;
 
-    while ( ! (mapped) || count != virtPageNum ) {
-      Page page = ( Page ) mem.elementAt( count );
-      if ( page.physical != -1 ) {
-        if (firstPage == -1) {
-          firstPage = count;
-        }
-        if (page.inMemTime > oldestTime) {
-          oldestTime = page.inMemTime;
-          oldestPage = count;
-          mapped = true;
-        }
+  public static void replacePage ( Vector mem , int virtPageNum , int replacePageNum , ControlPanel controlPanel )
+  {
+    int startPosition = pointer;
+    boolean mapped = false;
+    int removingPage = -1;
+    int lastMappedPage = -1;
+
+    for (int i = 0; i < mem.size(); i++) {
+      pointer++;
+      if (pointer == mem.size())
+        pointer = 0;
+
+      Page page = ( Page ) mem.elementAt( pointer );
+
+      if (page.physical == -1)      //do not account for not mapped pages
+        continue;
+      else
+        lastMappedPage = pointer;   //in case we do not find any page to remap, last one is remapped
+
+      if (page.R == 1)              //if page has R bit set, skip
+        continue;
+
+      if (page.lastTouchTime < TAU) //if page was touched in time less than tau, skip
+        continue;
+
+      if (page.M == 1) {            //if page was modified, skip but schedule write on disk
+        page.M = 0;                 //for testing we assume writing finishes over one full clock revolution
+        removingPage = pointer;     //we will use this page if not other page suits our needs after the search
+        continue;
       }
-      count++;
-      if ( count == virtPageNum ) {
-        mapped = true;
-      }
+
+      removingPage = pointer;       //pointer points to found page
+
+      break;                        //if no skip conditions met, remap found page
     }
-    if (oldestPage == -1) {
-      oldestPage = firstPage;
-    }
-    Page page = ( Page ) mem.elementAt( oldestPage );
+
+    if (removingPage == -1)
+      removingPage = lastMappedPage;
+
+    Page page = ( Page ) mem.elementAt( removingPage );
     Page nextpage = ( Page ) mem.elementAt( replacePageNum );
-    controlPanel.removePhysicalPage( oldestPage );
+
+    controlPanel.removePhysicalPage( removingPage );
+
     nextpage.physical = page.physical;
+
     controlPanel.addPhysicalPage( nextpage.physical , replacePageNum );
+
     page.inMemTime = 0;
     page.lastTouchTime = 0;
     page.R = 0;
